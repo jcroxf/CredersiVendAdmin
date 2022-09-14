defd4b84623633a3de253357520d94e263c8cf5d
Alec Riding <alec.riding@roq.co.uk>
2022-09-14 14:57:52 +0100

    Added Jest tests and postman tests and file setup for junit

---
+++ backend/src/test/java/com/credersi_vend/backend/routes/nodes/CustomerRequestTest.java
@@ -0,0 +1,40 @@
+//package com.credersi_vend.backend.routes.nodes;
+//
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertFalse;
+//import static org.junit.Assert.assertTrue;
+//import org.junit.jupiter.api.Test;
+//import org.neo4j.driver.AuthToken;
+//import org.neo4j.driver.AuthTokens;
+//import org.neo4j.driver.Driver;
+//import org.neo4j.driver.GraphDatabase;
+//import org.neo4j.driver.Record;
+//import org.neo4j.driver.Result;
+//import org.neo4j.driver.Session;
+//import org.neo4j.driver.Transaction;
+//import org.neo4j.driver.TransactionWork;
+//import org.neo4j.driver.Value;
+//import org.neo4j.driver.util.Pair;
+//import java.util.ArrayList;
+//import java.util.HashSet;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.Set;
+//import java.util.UUID;
+//import static org.neo4j.driver.Values.parameters;
+//import com.credersi_vend.routes.CredersiRoute;
+//import com.credersi_vend.routes.base.*;
+//import com.credersi_vend.routes.fluid.*;
+//import com.credersi_vend.routes.nodes.*;
+//import com.credersi_vend.routes.rels.*;
+//
+//
+
+//class ListCustomerTest {
+//	@Test
+//	public DomainNode getDomain() {
+//        return this.getDomain();
+//    }
+//}
+//
+
